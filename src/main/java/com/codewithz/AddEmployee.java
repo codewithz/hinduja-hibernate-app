a01a0964b97d3b48df5d575c715ae89495d66877
Zartab Nakhwa <zartab@codewithz.com>
2024-12-18 19:19:06 +0530

    Add Employee completed

--- src/main/java/com/codewithz/AddEmployee.java
+++ src/main/java/com/codewithz/AddEmployee.java
@@ -2,7 +2,11 @@ package com.codewithz;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class AddEmployee {
 
@@ -11,6 +15,20 @@ public class AddEmployee {
             Configuration cfg=new Configuration().configure();
             SessionFactory sessionFactory=cfg.buildSessionFactory();
             Session session=sessionFactory.openSession();
+
+            Transaction tx=session.beginTransaction();
+
+            Employee employee=new Employee();
+            employee.setName("Tom");
+            employee.setSalary(5000);
+            employee.setDept("IT");
+            employee.setDesignation("Developer");
+            employee.setDateOfJoin(new Date(2023, Calendar.FEBRUARY,23));
+
+            session.save(employee);
+            tx.commit();
+            System.out.println("Employee added successfully");
+            session.close();
 
         }
         catch (Exception e) {
